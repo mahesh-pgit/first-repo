23a2b62de164a1b744cee79ad9be0e587b6371a1
mahesh-pgit <naravamaheshsaikumar.321@gmail.com>
2024-10-22 22:38:59 +0530

    implemented Routes in Nav

--- src/App.js
+++ src/App.js
@@ -4,26 +4,58 @@ import Intro from "./components/Intro";
 import Footer from "./components/Footer";
 import ModeToggler from "./components/ModeToggler";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
 	return (
 		<div>
-			<ModeToggler />
-			<Nav />
-			<Promo />
-			<Intro
-				head="I've become a React developer!"
-				para="I've completed the React Basics course and I'm happy to announce that I'm now a Junior React Developer!"
-			/>
-			<Intro
-				head="Why I love front-end web development"
-				para="In this blog post, I'll list 10 reasons why I love to work as a front-end developer."
-			/>
-			<Intro
-				head="What's the best way to style your React apps?"
-				para="There are so many options to choose from. Here's a high-level overview of the popular ones."
-			/>
-			<Footer />
+			<Routes>
+				<Route path="/" element={<Nav />} />
+				<Route
+					path="/change-mode"
+					element={
+						<div>
+							<Nav /> <ModeToggler />
+						</div>
+					}
+				/>
+				<Route
+					path="/promo"
+					element={
+						<div>
+							<Nav /> <Promo />
+						</div>
+					}
+				/>
+				<Route
+					path="/intro"
+					element={
+						<div>
+							<Nav />
+							<Intro
+								head="I've become a React developer!"
+								para="I've completed the React Basics course and I'm happy to announce that I'm now a Junior React Developer!"
+							/>
+							<Intro
+								head="Why I love front-end web development"
+								para="In this blog post, I'll list 10 reasons why I love to work as a front-end developer."
+							/>
+							<Intro
+								head="What's the best way to style your React apps?"
+								para="There are so many options to choose from. Here's a high-level overview of the popular ones."
+							/>
+						</div>
+					}
+				/>
+				<Route
+					path="/footer"
+					element={
+						<div>
+							<Nav /> <Footer />
+						</div>
+					}
+				/>
+			</Routes>
 		</div>
 	);
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,36 @@
+import { Link } from "react-router-dom";
+
 function Nav() {
+	const navStyles = {
+		background: "indigo",
+		padding: "10px",
+		textAlign: "center",
+		fontSize: "20px",
+	};
+
+	const linkStyles = {
+		margin: "20px",
+		fontSize: "20px",
+		color: "white",
+		textDecoration: "none",
+	};
 	return (
-		<nav className="main-nav">
-			<ul>
-				<li>Home</li>
-				<li>Articles</li>
-				<li>About</li>
-				<li>Contact</li>
-			</ul>
+		<nav style={navStyles} className="main-nav">
+			<Link style={linkStyles} to="/">
+				Home
+			</Link>
+			<Link style={linkStyles} to="/change-mode">
+				Change Mode
+			</Link>
+			<Link style={linkStyles} to="/promo">
+				Promo
+			</Link>
+			<Link style={linkStyles} to="/intro">
+				Introduction
+			</Link>
+			<Link style={linkStyles} to="/footer">
+				Footer
+			</Link>
 		</nav>
 	);
 }
